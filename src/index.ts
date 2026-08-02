ec0fbc85d45f2b353c07f2a5d2799efd03ed4968
Julius Kertscher <dev.juke@proton.me>
2026-08-02 11:37:43 +0200

    restructured index.ts and extracted logic into onion/generate.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import {Result} from "./Utils/Result";
-import * as ed from '@noble/ed25519';
-import { sha512 } from '@noble/hashes/sha2.js';
-import { sha3_256 } from '@noble/hashes/sha3.js';
-import {base32Encode} from "./Utils/base32";
+import {createCheckSum, createKeyPair, createOnionAddress} from "./onion/generate";
 
 /**
  * Computes one random onion v3 address
@@ -10,49 +7,9 @@ import {base32Encode} from "./Utils/base32";
  */
 export function create(): Result {
     //https://github.com/torproject/torspec/blob/12271f0e6db00dee9600425b2de063e02f19c1ee/rend-spec-v3.txt#L2136-L2158
-
     const keyPair = createKeyPair();
     const checkSum = createCheckSum(keyPair.publicKey);
     const address = createOnionAddress(keyPair.publicKey, checkSum);
 
     return new Result(keyPair.publicKey.toString(), keyPair.secretKey.toString(), address);
-}
-
-function createOnionAddress(publicKey: Uint8Array, checkSum: Uint8Array): string {
-    // onion_address = base32(pubkey || checksum || version)
-    const onionAddressBytes = concatBytes(
-        publicKey,
-        checkSum.slice(0, 2),
-        new Uint8Array([0x03])
-    );
-
-    return base32Encode(onionAddressBytes).toLowerCase();
-}
-
-function createCheckSum(publicKey: Uint8Array): Uint8Array {
-    // checksum = H(".onion checksum" || pubkey || version)
-    const encoder = new TextEncoder();
-    const checksumBytes = concatBytes(
-        encoder.encode(".onion checksum"),
-        publicKey,
-        new Uint8Array([0x03])
-    );
-
-    return sha3_256(checksumBytes);
-}
-
-function concatBytes(...arrays: Uint8Array[]): Uint8Array {
-    const totalLength = arrays.reduce((sum, arr) => sum + arr.length, 0);
-    const result = new Uint8Array(totalLength);
-    let offset = 0;
-    for (const arr of arrays) {
-        result.set(arr, offset);
-        offset += arr.length;
-    }
-    return result;
-}
-
-function createKeyPair(): { secretKey: Uint8Array; publicKey: Uint8Array } {
-    ed.hashes.sha512 = sha512;
-    return ed.keygen();
 }
---
+++ src/onion/generate.ts
@@ -0,0 +1,43 @@
+﻿import * as ed from '@noble/ed25519';
+import { sha512 } from '@noble/hashes/sha2.js';
+import { sha3_256 } from '@noble/hashes/sha3.js';
+import {base32Encode} from "../Utils/base32";
+
+export function createOnionAddress(publicKey: Uint8Array, checkSum: Uint8Array): string {
+    // onion_address = base32(pubkey || checksum || version)
+    const onionAddressBytes = concatBytes(
+        publicKey,
+        checkSum.slice(0, 2),
+        new Uint8Array([0x03])
+    );
+
+    return base32Encode(onionAddressBytes).toLowerCase();
+}
+
+export function createCheckSum(publicKey: Uint8Array): Uint8Array {
+    // checksum = H(".onion checksum" || pubkey || version)
+    const encoder = new TextEncoder();
+    const checksumBytes = concatBytes(
+        encoder.encode(".onion checksum"),
+        publicKey,
+        new Uint8Array([0x03])
+    );
+
+    return sha3_256(checksumBytes);
+}
+
+export function concatBytes(...arrays: Uint8Array[]): Uint8Array {
+    const totalLength = arrays.reduce((sum, arr) => sum + arr.length, 0);
+    const result = new Uint8Array(totalLength);
+    let offset = 0;
+    for (const arr of arrays) {
+        result.set(arr, offset);
+        offset += arr.length;
+    }
+    return result;
+}
+
+export function createKeyPair(): { secretKey: Uint8Array; publicKey: Uint8Array } {
+    ed.hashes.sha512 = sha512;
+    return ed.keygen();
+}
